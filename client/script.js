64347d0da73b377db411a9129c348cfc1ce0e93e
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2022-12-25 17:18:09 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async(e) => {
 
   //fetch data from server 
 
-  const response = await fetch('https://codex-fln4.onrender.com', {
+  const response = await fetch('https://codex-fln4.onrender.com/', {
     method: 'POST',
     headers : {
       'Content-Type' : 'application/json'
